ced7fe60437e4895f3cff33ddd84fb0692ab3a09
justfrankie <zhuangfrankie@gmail.com>
2020-09-09 02:00:57 -0700

    created solution branch

--- server/controllers.js
+++ server/controllers.js
@@ -2,40 +2,15 @@ const modelHelpers = require('../database/helpers.js')
 
 let controllers = {
     getAll: (req, res) => {
-        modelHelpers.getAll((err, data) => {
-            if (err){
-                res.status(404).send(err);
-            } else {
-                res.status(200).send(data);
-            }
-        })
+
     }, 
     addOne: (req, res) => {
-        modelHelpers.addOne(req.body, (err, results) => {
-            if (err){
-                res.status(404).send(err);
-            } else {
-                res.status(200).send(`${req.body.restaurant} posted into the database`);
-            }
-        })
+
     },
     deleteAll: (req, res) => {
-        modelHelpers.deleteAll((err, results) => {
-            if (err){
-                res.status(404).send(err);
-            } else {
-                res.status(200).send('succesfully truncated table.');
-            }
-        })
+
     },
-    deleteOne: (req, res) => { // this is where we need req.params!!!!
-        modelHelpers.deleteOne( req.params.id, (err, results) => {
-            if (err) {
-                res.status(404).send(err);
-            } 
-            res.status(200).send(results)
-        })
-   }
+
 }
 
 module.exports = controllers;
--- server/index.js
+++ server/index.js
@@ -1,17 +1,1 @@
 const express = require('express');
-const app = express();
-const path = require('path');
-const router = require('./router.js');
-const port = 3333;
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-
-app.use('/', express.static(path.join(__dirname, '/../client/public')))
-app.use('/api', router)
-
-
-
-app.listen(port, () => console.log(`Successfully connected to Welp! on http://localhost:${port}`))
